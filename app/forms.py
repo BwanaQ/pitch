494316ae07076b8e0a727f5d4a9c62468a9bd76f
BwanaQ <thunjawax@gmail.com>
2021-03-02 18:20:34 +0300

    add-ft-registration

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms.validators import ValidationError, DataRequired, Email, Length, EqualTo
+from app.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -9,8 +10,18 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField(
-        'Password', validators=[DataRequired(), EqualTo('password')])
+        'Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
 
 
 class LoginForm(FlaskForm):
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, flash, redirect, url_for, request
 from app import app
+from app import db
 from app.forms import LoginForm, RegistrationForm
 from flask_login import login_required, logout_user, current_user, login_user
 from app.models import User
@@ -58,13 +59,18 @@ def login():
     return render_template('login.html', title='Sign In', form=form)
 
 
-@app.route('/registration')
+@app.route('/registration', methods=['GET', 'POST'])
 def registration():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash('Registration requested for username {}, email={}, password={}, confirm_password'.format(
-            form.username.data, form.email.data, form.password.data, form.confirm_password.data))
-        return redirect(url_for('index'))
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
 
     return render_template('registration.html', title='Sign Up', form=form)
 
